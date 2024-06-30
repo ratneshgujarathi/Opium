faedb95caebed75615259ea837e0a0ef16f7e813
Ratnesh Gujarathi <ratneshgujarathi@outlook.com>
2024-06-30 22:12:01 +0530

    Login page setup

---
+++ opium/src/pages/core/login/Login.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import "./login.css";
+
+function Login() {
+  return (
+    <div className='login-container container'>
+      <div class="left-half">
+            <div class="login-form">
+
+            </div>
+        </div>
+        <div class="right-half">
+            <img src="" alt="Right Half Image" />
+        </div>
+    </div>
+  )
+}
+
+export default Login
--- opium/src/routes/WebRouter.jsx
+++ opium/src/routes/WebRouter.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "../pages/core/login/Login";
 
 function WebRouter() {
     return (
         <BrowserRouter>
             <Routes>
                 {/* <Route path="/" element={<HomePage/>}></Route> */}
+                <Route path="/login" element={<Login/>}></Route>
             </Routes>
         </BrowserRouter>
     )
